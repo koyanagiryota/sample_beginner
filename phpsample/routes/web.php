5e8e65c575cbd22a602fccdbce0d2883f0b0de49
koyanagiryota <48145751+koyanagiryota@users.noreply.github.com>
2021-02-25 12:08:56 +0900

    fixed layout fo blade files

--- phpsample/routes/web.php
+++ phpsample/routes/web.php
@@ -10,10 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//ホーム画面
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
+//フォルダ作成画面
 Route::get('/folders/create','FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create','FolderController@create');
+
+//タスク作成画面
+Route::get('/folders/{id}/tasks/create','TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create','TaskController@create');
 
 Route::get('/', function() {
     return view('welcome');
